c8b8ff99f180cdfa1acaf8c7f0afcdf59c367ef7
javadghane <13731373>
2017-02-14 15:03:38 +0330

    add retrofit base api's

--- app/src/main/java/ir/javadroid/mvpsimpletest/MainActivity.java
+++ app/src/main/java/ir/javadroid/mvpsimpletest/MainActivity.java
@@ -19,35 +19,9 @@ public class MainActivity extends AppCompatActivity implements mainActivityPrese
         setContentView(R.layout.activity_main);
         presenter = new mainActivityPresenter(this);
 
-     /*"name": "a",
-      "depth": "0",
-      "id": "1",
-      "lft": "1",
-      "rgt": "18"*/
-        //    node a1 = new node(1, "a", new node())
         presenter.setCountryList(null);
     }
 
-    class node {
-        int id;
-        String name;
-        node right;
-        node left;
-
-        node(int id, String name, node right, node left) {
-            this.id = id;
-            this.name = name;
-            this.right = right;
-            this.left = left;
-        }
-
-        node(int id, node right, node left) {
-            this.id = id;
-            this.right = right;
-            this.left = left;
-        }
-
-    }
 
     @Override
     public void addCountries(ArrayList<country> countryList) {
